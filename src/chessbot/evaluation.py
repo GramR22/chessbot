087cffb64eff1f28cd457cdaa89dd78790744e19
GLTOP-Ubuntu <gtrman22@gmail.com>
2026-04-11 19:22:39 -0400

    built evaluator and search algo

--- src/chessbot/evaluation.py
+++ src/chessbot/evaluation.py
@@ -17,3 +17,31 @@ def evaluate_curr_pos(board: chess.Board) -> int:
         chess.KING: 0
     }
     
+    """     
+    wt = is white peice type
+    vt = peice value
+    ∑ wt⋅vt
+    """
+    white_score = sum([len(board.pieces(chess.PAWN, chess.WHITE)) * piece_value[chess.PAWN],\
+                        len(board.pieces(chess.KNIGHT, chess.WHITE)) * piece_value[chess.KNIGHT],\
+                        len(board.pieces(chess.BISHOP, chess.WHITE)) * piece_value[chess.BISHOP],\
+                        len(board.pieces(chess.ROOK, chess.WHITE)) * piece_value[chess.ROOK],\
+                        len(board.pieces(chess.QUEEN, chess.WHITE)) * piece_value[chess.QUEEN],\
+                        len(board.pieces(chess.KING, chess.WHITE)) * piece_value[chess.KING]])
+    """
+    bt = is black peice type
+    vt = peice value
+    ∑ wt⋅vt
+    """
+    black_score = sum([len(board.pieces(chess.PAWN, chess.BLACK)) * piece_value[chess.PAWN], \
+                        len(board.pieces(chess.KNIGHT, chess.BLACK)) * piece_value[chess.KNIGHT],\
+                        len(board.pieces(chess.BISHOP, chess.BLACK)) * piece_value[chess.BISHOP],\
+                        len(board.pieces(chess.ROOK, chess.BLACK)) * piece_value[chess.ROOK],\
+                        len(board.pieces(chess.QUEEN, chess.BLACK)) * piece_value[chess.QUEEN],\
+                        len(board.pieces(chess.KING, chess.BLACK)) * piece_value[chess.KING]])
+    
+    
+    # to give an evaluation of how the game is going we return white score-black score
+    return (white_score-black_score)
+
+print(evaluate_curr_pos(chess.Board()))
---
+++ src/chessbot/search.py
@@ -0,0 +1,66 @@
+from evaluation import evaluate_curr_pos as evaluator
+import chess
+"""
+board -  is our chess board
+depth -  is how many moves in the future do we check
+alpha -  is the lower bound of the score we can get
+beta  -  is the upper of the score we can get
+evalulator is our evaluation.py
+
+In this function it will recursivly check future moves maximizing the best move for both teams
+"""
+
+
+
+
+def negamax(board, depth, alpha, beta, evaluator):
+    if depth == 0 or board.is_game_over:
+        score = evaluator(board)
+        return score if board.turn == chess.WHITE else -score
+
+    #init something
+    best_move = -float("inf")
+    
+    for move in board.legal_moves:
+        board.push(move)
+        score = -negamax(board, depth - 1, -alpha, -beta, evaluator)
+        board.pop()
+
+    # is this move better than what we have if yes update
+        if score > best_move:
+            best = score
+    # have we passed our lower bound if yes update because we are sure we can get atleast this much
+        if best > alpha:
+            alpha = best
+    # if our best is greater than beta we exit because our opponent has beeten us beyond this
+        if best >= beta:
+            break
+
+    # this is our best move
+    return best
+
+
+def find_best_move(board, depth, evaluator, alpha = -float("inf"), beta = -float("inf")):
+    
+    #init our variables
+    best_move = None
+    best_score = -float("inf")
+
+    # once again we check the moves and scores to see if we have the best score
+    for move in board.legal_moves:
+        
+        # same as before
+        board.push(move)
+        score = -negamax(board, depth-1, -alpha, -beta, evaluator)
+        board.pop()
+        
+        # get best move based on score
+        if score > best_score:
+            best_score = score
+            best_move = move
+
+    return best_move
+
+
+print(find_best_move(chess.Board(), 4, evaluator))
+
