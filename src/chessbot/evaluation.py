e90d91b814c488f5e844fb5bfc878292205c9007
GLTOP-Ubuntu <gtrman22@gmail.com>
2026-04-16 18:00:19 -0400

    updated comments and king value

--- src/chessbot/evaluation.py
+++ src/chessbot/evaluation.py
@@ -5,8 +5,6 @@ import chess
 def evaluate_curr_pos(board: chess.Board) -> int:
     """
         Each peice is given a value based on how powerful it is in the game. 
-        Honestly not a chess pro so AI gave these values to me
-        WE NEED TO CHECK LATER
         """
     piece_value = {
         chess.PAWN: 100,
@@ -14,7 +12,7 @@ def evaluate_curr_pos(board: chess.Board) -> int:
         chess.BISHOP: 330,
         chess.ROOK: 500,
         chess.QUEEN: 900,
-        chess.KING: 0
+        chess.KING: 10000
     }
     
     """     
